b0060bd7577b2aefdd3995cdf7968b2794788733
Maksym Akulov <maksakulov93@gmail.com>
2016-08-04 16:55:57 +0300

    кнопки MainWindow

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -18,7 +18,6 @@ namespace DBO.ViewModel
             {
                 frameSource = value;
                 OnPropertyChanged("FrameSource");
-                //nhfnfhfhf
             }
         }
 
